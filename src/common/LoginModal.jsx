89da037a32ebdbb4db9c394eacea18bab211b108
Andy <andyzhanggm@gmail.com>
2024-01-03 23:09:52 -0500

    Implemented OAuth (kinda), had some issues with Modal not closing so needed to use setState with ternary to hide/show the modal

--- src/common/LoginModal.jsx
+++ src/common/LoginModal.jsx
@@ -1,15 +1,50 @@
-import { GoogleLogin } from "@react-oauth/google";
+import { useGoogleLogin, googleLogout } from "@react-oauth/google";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const LoginModal = () => {
-  const onSuccess = (res) => {
-    console.log("LOGIN SUCCESS Current User: ", res);
+  const [showModal, setShowModal] = useState(false);
+  const [profile, setProfile] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (profile) {
+      const { access_token } = profile;
+      axios
+        .get(
+          `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${access_token}`,
+          {
+            headers: {
+              Authorization: `Bearer ${access_token}`,
+              Accept: "application/json",
+            },
+          }
+        )
+        .then((res) => {
+          setProfile(res.data);
+        })
+        .catch((err) => console.error(err));
+    }
+  }, [profile]);
+
+  const logout = () => {
+    googleLogout();
+    setProfile(null);
+    navigate("/");
   };
 
-  const onFailure = (res) => {
-    console.log("LOGIN FAILED res: ", res);
-  };
+  const login = useGoogleLogin({
+    onSuccess: (response) => {
+      setProfile(response);
+      navigate("/home");
+      setShowModal(false);
+    },
+    onError: (error) => console.error("Login Failed:", error),
+  });
+
   const handleSubmit = (e) => {
-    e.preventDefault(); // Prevents the default form submission behavior
+    e.preventDefault();
 
     // Fetch form data or perform any other action here
     const username = document.getElementById("exampleInputUsername").value;
@@ -24,84 +59,104 @@ const LoginModal = () => {
 
   return (
     <>
-      <a
-        href='#'
-        className='btn btn-primary rounded-pill px-4 mx-4 shadow-sm fs-5'
-        data-bs-toggle='modal'
-        data-bs-target='#exampleModal'
-      >
-        Login
-      </a>
-
-      <div
-        className='modal fade'
-        id='exampleModal'
-        tabIndex='-1'
-        aria-labelledby='exampleModalLabel'
-        aria-hidden='true'
-      >
-        <div className='modal-dialog py-5 w-100'>
-          <div className='modal-content'>
-            <form onSubmit={(e) => handleSubmit(e)}>
-              <div className='modal-header'>
-                <h1
-                  className='modal-title fs-5'
-                  id='exampleModalLabel'
-                >
-                  Login with your existing account
-                </h1>
-                <button
-                  type='button'
-                  className='btn-close'
-                  data-bs-dismiss='modal'
-                  aria-label='Close'
-                ></button>
-              </div>
-              <div className='modal-body'>
-                <div className='mb-5'>
-                  <label
-                    htmlFor='exampleInputUsername'
-                    className='form-label d-flex fs-3'
-                  >
-                    Username
-                  </label>
-                  <input
-                    type='username'
-                    className='form-control'
-                    id='exampleInputUsername'
-                    aria-describedby='usernameHelp'
-                  />
-                </div>
-                <div className='mb-5'>
-                  <label
-                    htmlFor='exampleInputPassword1'
-                    className='form-label d-flex fs-3'
-                  >
-                    Password
-                  </label>
-                  <input
-                    type='password'
-                    className='form-control'
-                    id='exampleInputPassword1'
-                  />
-                </div>
-              </div>
-              <div className='modal-footer'>
-                <GoogleLogin
-                  onSuccess={onSuccess}
-                  onFailure={onFailure}
-                />
-                <button
-                  type='submit'
-                  className='btn btn-primary'
-                >
-                  Log in
-                </button>
-              </div>
-            </form>
-          </div>
+      {profile ? (
+        <div>
+          <img
+            src={profile.picture}
+            alt='user image'
+          />
+          <h3>User Logged in</h3>
+          <p>Name: {profile.name}</p>
+          <p>Email Address: {profile.email}</p>
+          <br />
+          <br />
+          <button onClick={logout}>Log out</button>
         </div>
-      </div>
+      ) : (
+        <div>
+          <a
+            href='#'
+            className='btn btn-primary rounded-pill px-4 mx-4 shadow-sm fs-5'
+            onClick={() => setShowModal(true)}
+          >
+            Login
+          </a>
+
+          {showModal && (
+            <div
+              className='modal fade show'
+              id='exampleModal'
+              tabIndex='-1'
+              aria-labelledby='exampleModalLabel'
+              aria-hidden='true'
+              style={{ display: "block" }}
+              onClick={() => setShowModal(false)}
+            >
+              <div className='modal-dialog'>
+                <div className='modal-content'>
+                  <form onSubmit={(e) => handleSubmit(e)}>
+                    <div className='modal-header'>
+                      <h1 className='modal-title fs-5'>
+                        Login with your existing account
+                      </h1>
+                      <button
+                        type='button'
+                        className='btn-close'
+                        data-bs-dismiss='modal'
+                        aria-label='Close'
+                        onClick={() => setShowModal(false)}
+                      ></button>
+                    </div>
+                    <div className='modal-body'>
+                      <div className='mb-5'>
+                        <label
+                          htmlFor='exampleInputUsername'
+                          className='form-label d-flex fs-3'
+                        >
+                          Username
+                        </label>
+                        <input
+                          type='username'
+                          className='form-control'
+                          id='exampleInputUsername'
+                          aria-describedby='usernameHelp'
+                        />
+                      </div>
+                      <div className='mb-5'>
+                        <label
+                          htmlFor='exampleInputPassword1'
+                          className='form-label d-flex fs-3'
+                        >
+                          Password
+                        </label>
+                        <input
+                          type='password'
+                          className='form-control'
+                          id='exampleInputPassword1'
+                        />
+                      </div>
+                    </div>
+                    <div className='modal-footer'>
+                      <button
+                        className='btn btn-primary rounded-pill px-4 mx-4 shadow-sm fs-5'
+                        onClick={login}
+                      >
+                        Sign in with Google
+                      </button>
+                      <button
+                        type='submit'
+                        className='btn btn-primary rounded-pill px-4 mx-4 shadow-sm fs-5'
+                      >
+                        Log in
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      )}
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,9 @@ import App from "./App.jsx";
 import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <GoogleOAuthProvider clientId='852112352719-14m9sl97i39vs6kegjj0d9bc0chiu0kg.apps.googleusercontent.com'>
-    <React.StrictMode>
+  <React.StrictMode>
+    <GoogleOAuthProvider clientId='852112352719-14m9sl97i39vs6kegjj0d9bc0chiu0kg.apps.googleusercontent.com'>
       <App />
-    </React.StrictMode>
-  </GoogleOAuthProvider>
+    </GoogleOAuthProvider>
+  </React.StrictMode>
 );
