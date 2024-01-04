c4785598072b6f505fbf664a3f9da2362cfcf388
Andy <andyzhanggm@gmail.com>
2024-01-04 17:09:16 -0500

    implemented fetch from bank api get all customers

--- src/pages/Customer/Customer.jsx
+++ src/pages/Customer/Customer.jsx
@@ -1,5 +1,34 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const Customer = () => {
-  return <div>Hello world customer</div>;
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(() => {
+    const fetchCustomerData = async () => {
+      axios
+        .get(`https://localhost:7095/api/customers`)
+        .then((res) => setCustomers(res.data.data))
+        .catch((err) => console.log(err));
+    };
+    fetchCustomerData();
+  }, []);
+  return (
+    <div>
+      <h2>Customers</h2>
+      <ul>
+        {customers.map((customer) => (
+          <li
+            key={customer.id}
+            className='fs-3'
+          >
+            Customer {customer.id}: {customer.firstName}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
+//within the li, we will create a component that will go into specific customer and show name, and other info. check exalidraw.
 export default Customer;
